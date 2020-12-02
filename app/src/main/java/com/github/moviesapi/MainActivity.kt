78e367929b0f2d497a2c65ff627e04e1b5f777ff
Meghdad Yaghoubi <meghdadya@gmail.com>
2020-12-02 13:09:11 +0330

    UI Launch Config
    App Theme added to themes.xml
    Config navigation component
    Add required tags to activity_main.xml
    create toolbar_layout.xml
    create MoviesFragment.kt

--- app/src/main/java/com/github/moviesapi/MainActivity.kt
+++ app/src/main/java/com/github/moviesapi/MainActivity.kt
@@ -2,10 +2,18 @@ package com.github.moviesapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.github.moviesapi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
