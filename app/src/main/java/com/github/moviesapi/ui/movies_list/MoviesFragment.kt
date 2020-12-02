2c714cd73d91a666a5d6050e914c52da5a297ddb
Meghdad Yaghoubi <meghdadya@gmail.com>
2020-12-02 13:09:11 +0330

    UI Launch Config
    App Theme added to themes.xml
    Config navigation component
    Add required tags to activity_main.xml
    create toolbar_layout.xml
    create MoviesFragment.kt

--- app/src/main/java/com/github/moviesapi/ui/movies_list/MoviesFragment.kt
+++ app/src/main/java/com/github/moviesapi/ui/movies_list/MoviesFragment.kt
@@ -19,7 +19,7 @@ class MoviesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        return inflater.inflate(R.layout.fragment_movies, container, false)
     }
 
 }
